042a18ac4b9d0be98b0458723341fa4b8e57d333
escandon100 <olakunle23odunade@gmail.com>
2026-02-08 01:40:44 +0100

    update

--- src/components/Fleet/FilteredFleet.tsx
+++ src/components/Fleet/FilteredFleet.tsx
@@ -1,11 +1,13 @@
 import type React from "react"
 import { useEffect, useState } from "react"
 import { Swiper, SwiperSlide } from "swiper/react"
-import { Pagination } from "swiper/modules"
-
+import { Navigation , Pagination} from "swiper/modules"
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew"
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos"
 
 import "swiper/css"
 import "swiper/css/pagination";
+import "swiper/css/navigation"
 
 import FleetData from "./FleetData"
 import FleetItem from "./FleetItem"
@@ -29,12 +31,18 @@ const FilteredFleet: React.FC<FilteredFleetProps> = ({ activeTab }) => {
 
   return (
     <div className="relative mt-8 flex items-center  ">
-    
+      <button className="hidden sm:block fleet-prev absolute left-0 z-10 h-10 w-10 rounded-full bg-white shadow flex items-center justify-center hover:bg-gray-100">
+        <ArrowBackIosNewIcon fontSize="small" />
+      </button>
       <div className="w-full">
         <Swiper
-            modules={[Pagination]}
-              pagination={{ clickable: true }}
+            modules={[Navigation , Pagination]}
             spaceBetween={0}
+             pagination={{ clickable: true }}
+            navigation={{
+            prevEl: ".fleet-prev",
+            nextEl: ".fleet-next",
+          }}
             slidesPerView={1}
             breakpoints={{
               640: { slidesPerView: 2 },
@@ -56,7 +64,9 @@ const FilteredFleet: React.FC<FilteredFleetProps> = ({ activeTab }) => {
 
 
       </div>
-    
+      <button className="hidden md:block fleet-next absolute right-0 z-10 h-10 w-10 rounded-full bg-white shadow flex items-center justify-center hover:bg-gray-100">
+        <ArrowForwardIosIcon fontSize="small" />
+      </button>
     </div>
   )
 }
